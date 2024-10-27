e10fa8121767898f2da306555c39d268ee3a782b
Deatherngai <tom2000998@yahoo.com.hk>
2024-10-27 20:14:49 +0800

    first commit

---
+++ src/components/index/GridList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div class="col">
+            <div class="item">
+                <table>
+                    <tbody>
+                        <tr><td>Image</td></tr>
+                        <tr><td>CollectionItem</td></tr>
+                        <tr><td>Writer</td></tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/index/VerticalList.vue
@@ -0,0 +1,33 @@
+<template>
+    <ul>
+        <li>
+            <table>
+                <thead>
+                    <tr><th rowspan="2"></th><th>Collection Item</th><th>:</th><th>Collection Item</th></tr>
+                </thead>
+                <tbody>
+                    <tr><td>Writer</td><td>:</td><td>Writer</td></tr>
+                </tbody>
+            </table>
+        </li>
+        <li>
+            <table>
+                <thead>
+                    <tr><th rowspan="2"></th><th>Collection Item</th><th>:</th><th>Collection Item</th></tr>
+                </thead>
+                <tbody>
+                    <tr><td>Writer</td><td>:</td><td>Writer</td></tr>
+                </tbody>
+            </table>
+        </li>
+    </ul>
+</template>
+<style>
+table,tr,th,td{
+    border: 1px solid black;
+}
+ul{
+    list-style-type: none;
+    margin: auto;
+}
+</style>
