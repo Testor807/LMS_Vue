2d4c1442c43dc049e584931660fc477de54c780e
Deatherngai <tom2000998@yahoo.com.hk>
2024-11-06 14:26:20 +0800

    update Page

---
+++ src/components/BookReport/reportForm.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div>
+            <img src="#" />
+        </div>
+        <div>
+            <label>Item Name</label>
+        </div>
+        <div>
+            <label>Author</label>
+        </div>
+        <div>
+            <label>Publication</label><label>Publication Year</label>
+        </div>
+        <div>
+            <div><label>Reading Notes</label></div>
+            <div><textarea></textarea></div>
+        </div>
+        <div>
+            <button>Submit</button>
+        </div>
+    </div>
+</template>
---
+++ src/components/Profile.vue
@@ -0,0 +1,33 @@
+<template>
+<div id="left-side" style="background-color: green;width: 300px;border: 2px solid yellow;">
+    <div>
+        <div><img src="#" /></div>
+        <div>
+            <label>Name:</label>
+            <label>Account Type:</label>
+        </div>
+    </div>
+    <div>
+        Setting
+    </div>
+</div>
+<div id="right-side" style="background-color: yellow;width: auto;border: 2px solid green;">
+    <div>
+        Account Status
+    </div>
+    <div>
+        <div><button>Loan</button><button>Revise</button></div>
+        <div id="records">
+            Content
+        </div>
+    </div>
+</div>
+</template>
+<style>
+#left-side{
+    float: left
+}
+#right-side{
+    float: left
+}
+</style>
